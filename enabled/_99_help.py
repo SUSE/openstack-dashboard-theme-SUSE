3654d74f5a3af011996a0c817542dbc60f8748f2
Thomas Bechtold <tbechtold@suse.com>
2015-01-28 17:41:28 +0100

    Override the url for the help page
    (cherry picked from commit cb80032440c631cffcd21deb87b8f2606f7b9939)

--- enabled/_99_help.py
+++ enabled/_99_help.py
@@ -1,3 +1,6 @@
+from django.core.urlresolvers import reverse_lazy
+import horizon
+
 # The name of the dashboard to be added to HORIZON['dashboards']. Required.
 DASHBOARD = 'help'
 # If set to True, this dashboard will be set as the default dashboard.
@@ -6,3 +9,7 @@ DEFAULT = False
 ADD_EXCEPTIONS = {}
 # A list of applications to be added to INSTALLED_APPS.
 ADD_INSTALLED_APPS = ['openstack_dashboard.dashboards.help']
+
+UPDATE_HORIZON_CONFIG = {
+    'help_url': reverse_lazy("horizon:help:guides:index"),
+}
